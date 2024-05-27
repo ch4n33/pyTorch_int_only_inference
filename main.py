e05b4dd5125bec904f070878efea70bdd6e1724f
LimBell <nowwhd987@naver.com>
2024-05-27 22:15:27 +0900

    main.py cifar10 dataloader data load test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # 데이터 변환 정의
@@ -23,3 +25,39 @@ trainloader = torch.utils.data.DataLoader(
     shuffle=True,  # 데이터를 섞어서 로드
     num_workers=2  # 데이터 로드에 사용할 프로세스 수
 )
+
+# CIFAR-10 테스트 데이터셋 불러오기
+testset = torchvision.datasets.CIFAR10(
+    root='./data',
+    train=False,  # 테스트용 데이터셋
+    download=True,
+    transform=transform
+)
+
+testloader = torch.utils.data.DataLoader(
+    testset,
+    batch_size=64,
+    shuffle=False,
+    num_workers=2
+)
+
+# 데이터셋 클래스 이름 정의
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# 데이터셋 로드 확인
+def loadtest():
+    # 이미지를 보여주는 함수 정의
+    def imshow(img):
+        img = img / 2 + 0.5  # 정규화 해제
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+    
+    # 학습용 데이터셋에서 일부 이미지 가져오기
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # 이미지 보여주기
+    imshow(torchvision.utils.make_grid(images))
+    # 레이블 출력
+    print(' '.join('%5s' % classes[labels[j]] for j in range(8)))
