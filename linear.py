b69f85393a6523d57409b1caa056d7b44062f9cc
LimBell <nowwhd987@naver.com>
2024-05-29 02:23:41 +0900

    nan, 학습이 되지 않는 오류 해결하기 위해 로그 추가

--- linear.py
+++ linear.py
@@ -14,11 +14,40 @@ class Linear(nn.Module):
         self.weight_quantizer = weight_quantizer or Quantizer(8, RangeTracker())
         # bias는 32bit precision으로, quantization 할 필요가 없음
         self.QAT = False
+        self.quantized = False
 
     def forward(self, x):
+        if self.quantized:
+            # ?
+            return # quantized w, x을 
+        if (torch.isnan(x).any()):
+            print('x didnt have nan')
+            assert False
+        if (torch.isnan(self.weight).any()):
+            print('weight didnt have nan')
+            assert False
+        if (torch.isnan(self.bias).any()):
+            print('bias didnt have nan')
+            assert False
         if self.QAT:
             x = self.activation_quantizer(x)
             weight = self.weight_quantizer(self.weight)
         else:
             weight = self.weight
+        if (torch.isnan(x).any()):
+            print('x has nan')
+            assert False
+        if (torch.isnan(weight).any()):
+            print('weight has nan')
+            assert False
+        if (torch.isnan(self.bias).any()):
+            print('bias has nan')
+            assert False
+        
         return torch.matmul(x, weight.t()) + self.bias
+    
+    def enable_quantization(self):
+        self.QAT = True
+    
+    def disable_quantization(self):
+        self.QAT = False
