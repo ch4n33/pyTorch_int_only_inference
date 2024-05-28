46a1d58531f56542bed410e6713ac2d594e2a3b0
LimBell <nowwhd987@naver.com>
2024-05-29 02:26:17 +0900

    with 문법으로 fake quantization을 수행할 수 있게 해 주는 utility

---
+++ util.py
@@ -0,0 +1,17 @@
+from linear import *
+
+
+class QuantizationEnabler():
+
+    def __init__(self, model):
+        self.model = model
+
+    def __enter__(self):
+        for module in self.model.modules():
+            if isinstance(module, Linear):
+                module.enable_quantization()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for module in self.model.modules():
+            if isinstance(module, Linear):
+                module.disable_quantization()
